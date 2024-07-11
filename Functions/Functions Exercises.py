09273fd1dad743f2c865e0f5fb08a00948bcb4ac
Abishek Phuyal <abishek2040@proton.me>
2024-07-11 23:48:40 +0100

    # Functions chapter - commit 1: Some exercises

---
+++ Functions/Functions Exercises.py
@@ -0,0 +1,9 @@
+# Here is some exercises from the functions chapter.
+
+# 1. write a function to print out a message.
+
+def print_out():
+    """A simple function to print out a message"""
+    print("Hello, I am doing very well today how are you.")
+
+print_out()
--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,1 +1,22 @@
 # Functions are named block of code in python that are designed to do one specific job or task, that are available to
+""" Functions in simple terms are a block of code that is designed to do one specific task, they are helpful to tackle
+    repeatitive tasks. If you need to run the function you call the function with any parameters that it requires or without
+    it. """
+
+# Here's a simple example of a function in pyhton:
+
+def greet_user():
+    """This function prints out the hello message."""
+    print("Hello,how are you doing. ")
+
+greet_user() #Here we are calling the function, it runs the print statement.
+
+
+# Here's another function example that takes in a name as an input.
+
+def greet_user(name):
+    """A simple function that takes in a name, and prints out a greetings message."""
+    print(f"\nHello, {name} really nice to see you here today. ")
+
+greet_user("Abishek") # Calling the function and passing the argument: "Abishek" to the parameter: "name".
+greet_user("Lewis")
