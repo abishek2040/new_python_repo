afd27e9906a84610fd479b91a45de0094dc38783
Abishek Phuyal <abishek2040@proton.me>
2024-07-14 00:29:43 +0100

    Passing lists to a function in python.

--- more function exercises.py
+++ more function exercises.py
@@ -35,4 +35,103 @@ greet_users(one)
 # to work efficiently even when you're dealing with large amount of data.
 
 # Here's a simple example of modifying a list without using functions, we'll do it again using functions later.
+# a 3d printing company gets orders to print and stores it in a list, it moves the orders to completed list once it
+# has been printed.
+
+orders_received = ["Ball","Pen","Mouse","Computer","Keyboard","Bottle"]
+completed = []
+
+while orders_received:
+    current_orders = orders_received.pop()
+    print(f"\nPrinting {current_orders.title()}")
+    completed.append(current_orders)
+print(completed)
+print(orders_received)
+for orders in completed:
+    print(orders)
+
+# We are going to Re-organize this code, by using functions.
+
+def modify_lists(orders, completed_orders):
+    """This function takes a list as an argument, and transfer all the elements to the other list. """
+    while orders:
+        current_orders = orders.pop()
+        print(f"Printing: {current_orders.title()}")
+        completed_orders.append(current_orders)
+
+def show_completed(completed_orders):
+    """This function displays all the elements from the completed_orders list. """
+    print(f"\nThe following models have printed. ")
+    for corders in completed_orders:
+        print(f"{corders}\n")
+
+orders = ["Ball","Pen","Book","Watch","Mouse","Phone"]
+completed_orders = []
+
+modify_lists(orders, completed_orders)
+show_completed(completed_orders)
+
+
+
+# Passing lists to a function: TRY IT YOURSELF:
+
+# 1. Make a list containing a series of short text messages. Pass the list to a function called show_messages(), which
+# prints each text message.
+
+messages = ["Hey", "hi", "hello","Gooday","Howdy","Namaste"]
+
+def show_message(messages):
+    """A function that takes in a list as an input and prints out the elements of the list."""
+    for message in messages:
+        print(message)
+
+show_message(messages)
+
+
+# Exercise - 2: Sending Messages - Move the messages to a different list:
+
+def move_messages(messages, sent_messages):
+    """Takes 2 lists as input and moves the messages from the messages argument to the sent_messages list."""
+    while messages:
+        current_message = messages.pop()
+        print(f"\nMoving Message: {current_message}")
+        sent_messages.append(current_message)
+
+def print_messages(sent_messages):
+    """Prints out the moved messages."""
+    for message in sent_messages:
+        print(message)
+
+messages = ["Hey", "hi", "hello","Gooday","Howdy","Namaste"]
+sent_messages = []
+
+move_messages(messages, sent_messages)
+print_messages(sent_messages)
+
+print(messages)
+print(sent_messages)
+
+
+# Exercise - 3: Not permanently changing the list - Making a copy of the original list without modifying it.
+
+def move_messages(messages, sent_messages):
+    """Takes 2 lists as input and moves the messages from the messages argument to the sent_messages list."""
+    while messages:
+        current_message = messages.pop()
+        print(f"\nMoving Message: {current_message}")
+        sent_messages.append(current_message)
+
+def print_messages(sent_messages):
+    """Prints out the moved messages."""
+    for message in sent_messages:
+        print(message)
+
+messages = ["Hey", "hi", "hello","Gooday","Howdy","Namaste"]
+sent_messages = []
+
+move_messages(messages[:], sent_messages)
+print_messages(sent_messages[])
+
+print(messages)
+print(sent_messages)
 
