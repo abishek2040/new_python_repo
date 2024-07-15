dea9962982982997bdba529db2776de8c7788395
Abishek Phuyal <abishek2040@proton.me>
2024-07-16 00:31:11 +0100

    Args & Kwargs Exercises

--- ARGS & KWARGS.py
+++ ARGS & KWARGS.py
@@ -10,3 +10,55 @@ def make_pizza(*toppings):
 
 make_pizza("Cheese")
 make_pizza("Pineapple","Ham","Chicken")
+
+
+# Using Arbitrary Keyword Arguments:
+# sometimes you'll want to accept an arbitrary number of arguments, but you won't know ahead of time what kind of info,
+# will be passed to the function, in this case you can pass as many key-value pairs as the function requires.
+
+# for example: Building User Profiles Functions
+
+def build_profiles(first, last, **user_info):
+    """Building a dictionary about a user."""
+    user_info["First Name"] = first
+    user_info["Last Name"] = last
+    return user_info
+
+user_profile = build_profiles("Abishek","Phuyal", location = "London", Age = 22)
+print(user_profile)
+
+
+# TRY IT YOURSELF:
+
+def make_sandwich(*filling):
+    """A function that asks for the filling on a sandwich and prints. """
+    for items in filling:
+        print(f"Adding {items} to your sandwich\n")
+make_sandwich("Lettuce","Cheese","Chicken")
+make_sandwich("Tuna","Onion","Cheese")
+make_sandwich("Chicken","Sweet corn")
+
+
+# User profiles:
+def user(first_name, last_name, **info):
+    """Build a user profile using arbitrary kwargs"""
+    info["first name"] = first_name
+    info["last_name"] = last_name
+    return info
+
+captain = user("Bruno", "Fernandes", Nationality="Portugal", Age = 26)
+print(captain)
+
+
+# 3. Cars:
+
+def cars(manufacturer, model_name, **misc):
+    """Car info - function """
+    misc["Manufacturer"] = manufacturer
+    misc["Model Name"] = model_name
+    return misc
+
+japan = cars("Toyota","Prius", Color="White", Purpose = "Uber")
+print(japan)
+Italy = cars("Ferrari", "Spyder", Color = "Black", Purpose= "Sport")
+print(Italy)
