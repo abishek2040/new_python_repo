12f6bc2b70f6033555270ae275844b53f71d4801
Abishek Phuyal <abishek2040@proton.me>
2024-08-05 23:19:56 +0100

    working with files in python.

--- Files & Exceptions/files & Exceptions.py
+++ Files & Exceptions/files & Exceptions.py
@@ -16,3 +16,14 @@ print(contents)
 with open("text.txt") as txt:
     for lines in txt:
         print(lines)
+
+
+# Writing to a file: To write a text file, you need to call open() with a second argument telling Python that you wish
+# to write to a file. Let's see an example of how to write a file in python.
+
+with open("hello.txt", "a") as new_file:
+    new_file.write("\nWe added this line to the hello.txt file by opening in append mode. ")
+
+"""It tells python that you'd like to open the file 'hello.txt' file in write mode, if the file doesn't already exists, 
+python will create a new file and if the file exists with the same name, it deletes everything from the file."""
+
