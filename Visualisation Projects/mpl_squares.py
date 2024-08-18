77dc9379a3b7b77d933b7fa1c919d8b7e28b6555
Abishek Phuyal <abishek2040@proton.me>
2024-08-18 01:51:57 +0100

    Visualisation

--- Visualisation Projects/mpl_squares.py
+++ Visualisation Projects/mpl_squares.py
@@ -7,5 +7,16 @@ cubes = [1, 8, 27, 64, 125]
 fig, (ax,ay) = plt.subplots(2)
 ax.plot(squares)
 ay.plot(cubes)
+ax.plot(squares, linewidth=3)
+ay.plot(cubes, linewidth=3)
+ax.set_title("Squares", fontsize=13)
+ax.set_xlabel("Value", fontsize=13)
+ax.set_ylabel("Square of Value", fontsize=13)
+ay.set_xlabel("Value", fontsize=13)
+ay.set_ylabel("Cube of Value", fontsize=13)
+ay.set_title("Cubes" , fontsize=13)
+
+ax.tick_params(axis='both', labelsize=13)
+
 plt.show()
 
