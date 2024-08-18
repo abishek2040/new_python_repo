dd6df647f4213bd67dce87ae4e11282a4c5e38df
Abishek Phuyal <abishek2040@proton.me>
2024-08-18 01:45:34 +0100

    Subplot 2, 2 visualisation in 1 figure

--- Visualisation Projects/mpl_squares.py
+++ Visualisation Projects/mpl_squares.py
@@ -2,8 +2,10 @@
 
 import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
+cubes = [1, 8, 27, 64, 125]
 
-fig, ax = plt.subplots()
+fig, (ax,ay) = plt.subplots(2)
 ax.plot(squares)
+ay.plot(cubes)
 plt.show()
 
